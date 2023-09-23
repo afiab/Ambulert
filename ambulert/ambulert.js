a0d105edb0e22519b4816c70c7eeb4a7ef129f2b
afiab <abidica30@stuy.edu>
2023-09-23 19:30:37 -0400

    drew squares woohoo

--- ambulert/ambulert.js
+++ ambulert/ambulert.js
@@ -5,33 +5,48 @@ let board = []
 
 let xCor = [];
 let yCor = [];
-for (let x=0; x<50; x++){
+//initializing array with board colors
+for (let x=0; x<=50; x++){
   let temp = []
   if (x%5 == 0){ //horizontal roads
-    for (let y=0; y<50; y++) {
-      temp.append(0);
+    for (let y=0; y<=50; y++) {
+      temp.push(true);
     }
   }else { //vertical
-    for (let y=0; y<50; y++) {
+    for (let y=0; y<=50; y++) {
       if (y%5 == 0) { //road
-        temp.append(0);
+        temp.push(true);
       }
       else { //sidewalk
-        temp.append(1);
+        temp.push(false);
       }
     }
   }
+  board.push(temp)
 }
+//end initializing
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(510,510);
   frameRate(15);
   stroke(255);
-  
   
 }
 
 
 function draw() {
-
+  //draw the city
+  for (let x=0;x<board.length;x++){
+    for (let y=0;y<board.length;y++){
+      if (board[x][y]){
+        fill('black');
+        square(x*10,y*10,50);
+      }else{
+        fill('gray');
+        square(x*10,y*10,50);
+      }
+    }
+  }
+  //done drawing city
+  
 }
