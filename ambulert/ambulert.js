59fb239e6636d7b23890814b9fb8f38edf2a72db
afiab <abidica30@stuy.edu>
2023-09-23 21:00:47 -0400

    create a class for pointer-shapes, which would be the ambulances and the player's car

--- ambulert/ambulert.js
+++ ambulert/ambulert.js
@@ -5,6 +5,15 @@ let board = []
 
 let xCor = [];
 let yCor = [];
+
+let bluex = 250;
+let bluey = 450;
+let red1x = 0;
+let red1y = 100;
+let red2x = 150;
+let red2y = 300;
+let red3x = 500;
+let red3y = 50;
 //initializing array with board colors
 for (let x=0; x<=50; x++){
   let temp = []
@@ -26,6 +35,9 @@ for (let x=0; x<=50; x++){
 }
 //end initializing
 
+let blueImage;
+let redImage;
+
 function setup() {
   createCanvas(510,510);
   frameRate(15);
@@ -33,15 +45,31 @@ function setup() {
   
 }
 
+class Pointer {
+  constructor(x, y, z) {
+    this.x = x+5;
+    this.y = y;
+    this.z = z;
+  }
+  display() {
+    fill(this.z);
+    stroke(this.z);
+    triangle(this.x-5,this.y-10,this.x,this.y,this.x,this.y-7.5);//left
+    triangle(this.x,this.y-7.5,this.x,this.y,this.x+5,this.y-10);
+    stroke('white');
+  }
+}
 
 function draw() {
   //draw the city
   for (let x=0;x<board.length;x++){
     for (let y=0;y<board.length;y++){
       if (board[x][y]){
+        stroke('black');
         fill('black');
         square(x*10,y*10,50);
       }else{
+        stroke('gray');
         fill('gray');
         square(x*10,y*10,50);
       }
@@ -49,4 +77,13 @@ function draw() {
   }
   //done drawing city
   
+  //test pointer
+  let red1 = new Pointer(red1x, red1y,'red');
+  red1.display()
+  let red2 = new Pointer(red2x, red2y,'red');
+  red2.display()
+  let red3 = new Pointer(red3x, red3y,'red');
+  red3.display()
+  let blue = new Pointer(bluex, bluey,'cyan');
+  blue.display()
 }
